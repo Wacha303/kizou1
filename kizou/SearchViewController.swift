dfa8bf81bf3915b98568f5a27d991af99def1275
Yuta <coffeebeans.77.black@gmail.com>
2019-02-16 18:33:33 +0900

    add new file

---
+++ kizou/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  kizou
+//
+//  Created by 中原雄太 on 2019/02/16.
+//  Copyright © 2019年 中原雄太. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ kizou/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  kizou
+//
+//  Created by 中原雄太 on 2019/02/14.
+//  Copyright © 2019年 中原雄太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
